4b0353305cb64f0fe2d3ca2733527d80adbdcd95
William Rodgers <william.rodgers@stu.ocsb.ca>
2024-10-16 11:11:58 -0400

    Godd for Now

--- main.js
+++ main.js
@@ -11,3 +11,72 @@
 
 'use strict';
 
+// Part 1
+
+function to_fahrenheit(celsius) {
+
+    let Fahrenheit = (celsius * 9/5) + 32;
+
+    return Fahrenheit;
+
+}
+
+// Part 2
+
+function to_celsius(fahrenheit) {
+
+    let Celcius = (fahrenheit - 32) * 5/9;
+
+    return Celcius;
+
+}
+
+// Part 3
+
+function roll_dice(Num, Sides) {
+
+    for (Num=Num; Num > 0; Num--) {
+
+    let Value = Math.floor(Math.random() * Sides + 1);
+
+    console.log(`You rolled ` + Value);
+    
+    }
+        
+}
+
+// Part 4
+
+function area_circle(Radius) {
+
+return Math.PI * Radius * 2;
+
+}
+
+// Part 5
+
+function area_cylinder(Radius, Height) {
+
+    return Math.PI * Radius ** 2 * Height;
+    
+    }
+
+// Part 6
+
+function pythagorean_theorem(a, b) {
+
+return Math.sqrt(a**2 + b**2);
+
+}
+
+function percent(Score, Total) {
+
+return Math.round(Score / Total * 100) + `%`;
+
+}
+
+function round(value, decimals) {
+
+return Math.round(value * (10 ** decimals)) / 10 ** decimals;
+
+}
